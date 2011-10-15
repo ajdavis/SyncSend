07c7065d620a9b9235febe18c8321171fde6be83
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2011-10-14 22:11:00 -0400

    Multichannel streaming I/O: try this in 5 console windows. window 1: python syncsend.py window 2: curl -T some_file localhost:8000 window 3: curl localhost:8000/some_file window 4: curl -T some_other_file localhost:8000 window 5: curl localhost:8000/some_other_file ... regardless of which order you run the commands in windows 2 through 5, the file is transferred.

--- syncsend.py
+++ syncsend.py
@@ -5,77 +5,98 @@ from twisted.web import http
 
 class SyncSendContent:
     """
-    Act like a file
+    Takes data written by a send request and writes it to a receive request
     """
-    def __init__(self, request):
+    def __init__(self, request, key):
+        """
+        Prepare to send a file from a sender to a receiver.
+        @param request:     The sender's POST or PUT request
+        @param key:         The sender's email address or some other unique key
+        """
+        assert request.channel._command in ('POST', 'PUT')
         self.request = request
+        self.key = key
+
+        # When we receive the first chunk of an uploaded file, if the receiver hasn't connected
+        # yet, we'll need to store that first chunk while we wait for the receiver to connect.
         self.buffer = None
 
     def seek(self, *args, **kwargs):
+        """
+        Request.requestReceived() calls seek(0,0), so stub it out here to avoid an AttributeError
+        """
         pass
 
     def write(self, data):
-        if get_request:
+        """
+        Receive data from the sender. If the receiver has connected, send the data, otherwise
+        buffer this chunk of data and pause sending until the receiver connects.
+        """
+        import time
+        time.sleep(1)
+        if self.key in get_requests:
+            get_request = get_requests[self.key]
+            print '%s: writing %s' % (
+                id(self.request),
+                (len(self.buffer) if self.buffer is not None else 0) + len(data),
+            )
             if self.buffer is not None:
                 # We have just resumed producing
                 get_request.write(self.buffer)
                 self.buffer = None
             get_request.write(data)
         else:
-            print 'channel id', id(self.request.channel)
             self.buffer = data
             self.request.channel.pauseProducing()
 
     def close(self, *args, **kwargs):
-        global get_request
-        if get_request:
-            get_request.finish()
-            get_request = None
+        get_requests[self.key].finish()
+        del get_requests[self.key]
         del self.request # Break circular reference to aid garbage collection
 
-post_request = None
-get_request = None
+# Users start sending or receiving files by entering an email address or
+# some other unique key. These dicts map from keys to GET requests (the
+# receivers) and from keys to PUT or POST requests (the receivers)
+post_requests = {}
+get_requests = {}
 
 class SyncSendRequest(http.Request):
-#    def gotLength(self, length):
-#        print 'length: %s' % length
-#        http.Request.gotLength(self, length)
-
     def gotLength(self, length):
-        global post_request
-
-        self.contentSoFar = 0
-        self.content = SyncSendContent(self)    
+        """
+        This request could either be the sender's POST or PUT request, or it could be the receiver's
+        GET request. Either way, set my key based on the URI.
+        """
+        # HACK: access internal variable, since self.path isn't set until all content has been uploaded
+        self.key = self.channel._path
+        print self.channel._command, self.key
 
         # HACK: access internal variable, since self.method isn't set until all content has been uploaded
         if self.channel._command in ('PUT', 'POST'):
-            post_request = self
-
-    def handleContentChunk(self, data):
-        self.contentSoFar += len(data)
-        print 'chunk: %s, %s to go' % (len(data), self.channel.length - self.contentSoFar)
-        http.Request.handleContentChunk(self, data)
-#        if self.contentSoFar >= self.channel.length:
-#            self.channel.allContentReceived()
-#            self.write('done\n')
-#            self.finish()
+            # Instead of using a StringIO or temp file as Twisted does, make a fake file object that
+            # will funnel data from the sender to the receiver
+            self.content = SyncSendContent(self, self.key)
+            post_requests[self.key] = self
+        else:
+            # Normal processing for GET requests
+            http.Request.gotLength(self, length)
 
     def process(self):
         """
         If we're processing an upload, this method is called when all content has been uploaded. If we're
         processing a download, this is called when we receive the whole GET request.
         """
-        global get_request, post_request
         if self.method in ('POST', 'PUT'):
             self.write('done\n')
+            del post_requests[self.key]
             self.finish()
-            post_request = None
-        elif self.method == 'GET':
-            get_request = self
-            if post_request:
-                print 'channel id', id(post_request.channel)
-                post_request.channel.resumeProducing()
             return twisted.web.server.NOT_DONE_YET
+        elif self.method == 'GET':
+            get_requests[self.key] = self
+            if self.key in post_requests:
+                post_requests[self.key].channel.resumeProducing()
+            return twisted.web.server.NOT_DONE_YET
+        else:
+            self.setResponseCode(405, 'Method not allowed')
 
 class SyncSendHttp(http.HTTPChannel):
     requestFactory = SyncSendRequest
